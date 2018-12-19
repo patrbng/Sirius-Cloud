f7d14d00ff592e47e32d5a5b5d577953ac280181
Jacob Benson <vhusker@users.noreply.github.com>
2018-12-19 07:18:08 -0600

    windows vm scaffolding

---
+++ QuickStart Scaffolding/ARM Templates/Windows VM/deploy.ps1
@@ -0,0 +1,27 @@
+Login-AzAccount
+Get-AzSubscription
+#This selects the free trial subscription
+Select-AzSubscription -Subscription '<appropriate subscription id>'
+
+#Create a new resource group if it doesn't exist already
+New-AzResourceGroup -Name "<resource group name>" -Location "<location>"
+
+#Update the file paths if you need to
+$rg = "<resource group name>"
+$template = "local path to template file"
+$parms = "local path to template parameters file"
+#You can give the job a meaningful name if you wish
+$job = 'job.' + ((Get-Date).ToUniversalTime()).tostring("MMddyy.HHmm")
+
+#Test the deployment
+Test-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $parms -Mode Incremental -Verbose
+
+#If the test deployment fails and you need more information save the information to a variable $test.  Look at the results in  $test.details
+$test = Test-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $parms -Mode Incremental -Verbose
+$test.Details
+
+#Deploy the template
+New-AzResourceGroupDeployment -Name $job -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $parms -Mode Incremental -DeploymentDebugLogLevel All -Verbose
+
+#Clean up the resources
+Remove-AzResourceGroup -Name $rg 
---
+++ QuickStart Scaffolding/ARM Templates/Windows VM/readme.md
@@ -0,0 +1,19 @@
+# VPN Gateway
+
+* This is a base VPN Gateway ARM Template that deploys a VPN Gateway and its required resources only
+* This template can be used by itself or as part of a larger deployment
+
+## Resources Provisioned In This Template
+
+* VPN Gateway
+* Public IP Address for VPN Gateway
+* Virtual Network with a Gateway Subnet
+
+## Information Required to Provision Resources
+
+* Naming prefix for the resources
+
+## Parameter Values Which Can Be Changed
+
+* VPN Gateway SKU
+* Naming Prefix
